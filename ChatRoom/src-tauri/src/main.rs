62ffe23064a484b7ecd665dd2223b047273d7748
琥珀飞蛾 <363473022@qq.com>
2024-12-27 01:51:02 +0800

    添加数据库登录后端代码

--- ChatRoom/src-tauri/src/main.rs
+++ ChatRoom/src-tauri/src/main.rs
@@ -3,6 +3,7 @@ use std::io::Write;
 use std::net::UdpSocket;
 use std::path::Path;
 use std::thread;
+use mysql::prelude::Queryable;
 use serde::Deserialize;
 use serde::Serialize;
 use tauri::Manager;
@@ -12,6 +13,7 @@ use std::sync::Mutex;
 use std::fs::{File, OpenOptions};
 use std::io::Read;
 use serde_json;
+use mysql::{Pool, PooledConn, params};
 
 
 struct AppState {
@@ -19,9 +21,109 @@ struct AppState {
 }
 
 #[derive(Serialize,Deserialize)]
-struct save_data{
+struct SaveData{
     local_port: u16,
 }
+
+#[derive(Debug, PartialEq, Eq)]
+struct User {
+    user_name: String,
+    user_password: String,
+}
+
+
+
+fn query_map_mysql(mut conn:PooledConn) ->Vec<User>{
+    match conn.query_map(
+        "SELECT user_name, user_password FROM users",
+        |(user_name, user_password)|{
+            User {user_name, user_password}
+        }
+    ){
+        Ok(rows) => {
+            return rows;
+        }
+        Err(e) => {
+            println!("Failed to query database: {}", e);
+            println!("trying to create database");
+            conn.query_drop(r"CREATE TABLE users(
+                user_id int not null AUTO_INCREMENT,
+                user_name varchar(255) not null,
+                user_password varchar(255) not null,
+                PRIMARY KEY (user_id)
+            )").expect("Failed to create table");
+            return query_map_mysql(conn);
+        }
+    }
+}
+
+fn insert_mysql(mut conn:PooledConn, user_name: &str, user_password: &str){
+    let user=&vec![User{user_name: user_name.to_string(), user_password: user_password.to_string()}];
+    match conn.exec_batch(
+        r"INSERT INTO users (user_name, user_password)
+          VALUES (:user_name, :user_password)",
+          user.iter().map(|p: &User| params! {
+            "user_name" => &p.user_name,
+            "user_password" => &p.user_password,
+        })
+    )
+    {
+        Ok(_) => {
+            println!("Data inserted successfully");
+        }
+        Err(e) => {
+            println!("Failed to insert database: {}", e);
+            println!("trying to create database");
+            conn.query_drop(r"CREATE TABLE users(
+                user_id int not null AUTO_INCREMENT,
+                user_name varchar(255) not null,
+                user_password varchar(255) not null,
+                PRIMARY KEY (user_id)
+            )").expect("Failed to create table");
+        }
+    };
+}
+
+// find_in_mysql("root", "{密码}", "127.0.0.1", "3306", "testscheme", "abc", "123");
+#[tauri::command]
+fn find_in_mysql(
+    mysqlname: &str, 
+    mysqlpassword: &str, 
+    mysqlhost: &str,
+    mysqlport: &str, 
+    mysqldb: &str,
+    username: &str, 
+    password: &str) -> Result<bool, String> {
+    // 连接到 MySQL 数据库
+    let mysqlurl=format!("mysql://{}:{}@{}:{}/{}",mysqlname,mysqlpassword,mysqlhost,mysqlport,mysqldb);
+    let pool = Pool::new(mysqlurl.as_str()).expect("Failed to create pool"); 
+    let mut conn = pool.get_conn().expect("Failed to get connection from pool");
+    let rows = query_map_mysql(conn);
+
+    for row in rows {
+        if row.user_name.as_str() == username && row.user_password.as_str() == password {
+            return Ok(true);
+        }
+    }
+    return Ok(false);
+}
+//createuser_in_mysql("root", "{密码}", "127.0.0.1", "3306", "testdatabase", "abcd", "1243");
+#[tauri::command]
+fn createuser_in_mysql(
+    mysqlname: &str, 
+    mysqlpassword: &str, 
+    mysqlhost: &str, 
+    mysqlport: &str, 
+    mysqldb: &str, 
+    username: &str,  
+    password: &str) -> Result<(), String> {
+    let mysqlurl=format!("mysql://{}:{}@{}:{}/{}",mysqlname,mysqlpassword,mysqlhost,mysqlport,mysqldb);
+    let pool = Pool::new(mysqlurl.as_str()).expect("Failed to create pool"); 
+    let mut conn = pool.get_conn().expect("Failed to get connection from pool");
+    insert_mysql(conn, username, password);
+    Ok(())
+}
+
 
 #[tauri::command]
 fn set_local_port(port: u16, state: State<AppState>) -> Result<(), String> {
@@ -45,12 +147,12 @@ fn send_message(message: String, target: String, port: u16) -> Result<(), String
     Ok(())
 }
 
-fn get_saved_data() -> save_data {
+fn get_saved_data() -> SaveData {
     // 从文件中读取保存的数据
-    // 返回一个 save_data 结构体
+    // 返回一个 SaveData 结构体
     let savedfilepath="saved_data.json";
     if !Path::new(savedfilepath).exists() {
-        let data: save_data = save_data {
+        let data: SaveData = SaveData {
             local_port: 8080,
         };
         match serde_json::to_string(&data) {
@@ -86,7 +188,7 @@ fn get_saved_data() -> save_data {
             }
             Err(e) => {
                 println!("Failed to deserialize data: {}", e);
-                return save_data {
+                return SaveData {
                     local_port: 8080,
                 };
             }
@@ -110,7 +212,7 @@ fn set_local_port_savedfile(new_port: u16) -> Result<(), String> {
         return Err(format!("Failed to read file: {}", e));
     }
 
-    let mut data: save_data = match serde_json::from_str(&contents) {
+    let mut data: SaveData = match serde_json::from_str(&contents) {
         Ok(data) => data,
         Err(e) => return Err(format!("Failed to deserialize data: {}", e)),
     };
